1a145ce73b73b100ed3d8acb9bb637fc97d67ccb
Black_Oath <yassine1998walid@gmail.com>
2021-03-28 15:50:43 +0100

    First Commit

---
+++ lib/views/opening_screen.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class OpeningView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+
+    final logo = Image.asset(
+      "assets/logo.png",
+      height: mq.size.height / 2,
+    );
+
+    //Login Button
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {
+          print("Login Pressed");
+        },
+      ),
+    );
+
+    //Register Buttons
+    final registerButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: Colors.white,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Register",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.bold),
+        ),
+        onPressed: () {
+          print("Register Pressed");
+        },
+      ),
+    );
+
+    final buttons = Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          loginButton,
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 90),
+            child: registerButton,
+          ),
+        ]);
+
+    final text = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Text(
+          "SoS is here to help",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.blueAccent,
+              fontWeight: FontWeight.bold),
+        )
+      ],
+    );
+
+    return Scaffold(
+        body: Padding(
+            padding: EdgeInsets.all(36),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.only(top: 50),
+                ),
+                logo,
+                buttons,
+                text
+              ],
+            )));
+  }
+}
